9045c28ca47c2f6e3f8678cf9659599b05739172
Brandon Bryan <bshwag@gmail.com>
2024-05-06 10:52:05 -0400

    Initial commit

---
+++ src/main/java/org/brandon/articlekraftbackend/ArticleKraftBackendApplication.java
@@ -0,0 +1,13 @@
+package org.brandon.articlekraftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleKraftBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArticleKraftBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/brandon/articlekraftbackend/ArticleKraftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brandon.articlekraftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleKraftBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
